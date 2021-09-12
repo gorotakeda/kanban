f6550664bdc16164e532b7cfa65fb4ddbd1e9d75
gorotakeda <rugbytkg3@gmail.com>
2021-09-12 15:18:21 +0900

    Bootstrap導入完了

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,3 +1,6 @@
+import '../stylesheets/application';
+import "../js/hoge.js"
+
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
@@ -13,5 +16,6 @@ require("channels")
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
-// const images = require.context('../images', true)
+const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
